69b2fea1a2e3e77b7b341e2275fe75846533a986
tarsilla <tarsillacosta.lima@gmail.com>
2019-09-17 22:19:38 -0300

    1 Commit

---
+++ ClassQuest/src/test/java/br/com/classquest/ClassQuest/ClassQuestApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.classquest.ClassQuest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClassQuestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
